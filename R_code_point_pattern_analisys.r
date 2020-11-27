5fcf7e63ae1457a2008f0b10d7cf626a8b5e542f
carlonotari <73799325+carlonotari@users.noreply.github.com>
2020-11-27 10:57:53 +0100

    Update R_code_point_pattern_analisys.r

--- R_code_point_pattern_analisys.r
+++ R_code_point_pattern_analisys.r
@@ -116,8 +116,34 @@ plot(cases_map, col = cl)
 points(covid_planar)
 plot(coastline, add = T)
 
+#### 27/11
+###plotting points with different size related to covid data togheter with the interpolation 
 
+library(spatstat)
+library(rgdal)
 
+setwd("C:/lab/")
+covid <- read.table("covid_agg.csv", header=TRUE)
+head(covid)
 
+attach(covid) # if you do not attach: covid$lon
+covid_planar <- ppp(lon, lat, c(-180,180), c(-90,90))
 
+marks(covid_planar) <- cases 
+cases_map<-Smooth(covid_planar)
 
+cl <- colorRampPalette(c('lightpink2','lightsalmon','tomato1','red3','maroon'))(100)
+##to change colour: https://www.datanovia.com/en/blog/r-colors-amazing-resources-you-want-to-know/
+
+plot(cases_map, col = cl)
+
+install.packages("sf")
+library(sf)
+Spoints <- st_as_sf(covid, coords = c("lon", "lat"))
+plot(Spoints, cex=Spoints$cases, col = 'purple3', lwd = 3, add=T)
+
+cl <- colorRampPalette(c('lightpink2','lightsalmon','tomato1','red3','maroon'))(100)
+plot(cases_map, col = cl)
+plot(Spoints, cex=Spoints$cases/10000, col = 'purple3', lwd = 3, add=T)
+
+ 
