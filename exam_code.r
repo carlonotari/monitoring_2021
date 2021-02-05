9a8f88f7476d3834983c5717a09f369e37b90aad
carlonotari <73799325+carlonotari@users.noreply.github.com>
2021-02-05 18:56:47 +0100

    Update exam_code.r

--- exam_code.r
+++ exam_code.r
@@ -1,5 +1,56 @@
 # exam code 
 
+library(ncdf4)
+library(raster)
+
+setwd("C:/exam_m/")
+
+al99<- raster("1999.nc")
+al20<- raster("2020.nc")
+plot(al99)
+plot(al20)
+
+
+
+#zoom siberia 
+
+ext <- c(-150,150,40,150)
+zoom(al99, ext=ext)
+zoom(al20, ext=ext)
+
+ext <- c(-150,150,40,150)
+zoom(al20, ext=ext)
+
+
+clal99 <- colorRampPalette(c('blue','white','red'))(100)  #CAMBIA STI COLORI BASTA!!
+jan20 <- crop(jan20, ext)
+plot(jan20, col=cljan20, main="titolo")
+
+
+
+
+dift <- tjan - toct #differenza jennaio meno ottobre 
+cldif <- colorRampPalette(c('blue','white','red'))(100) # 
+plot(dift, col=cldif)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
  
 # nc data: we need library
@@ -28,3 +79,36 @@ plot(jan19_fire, col=cljan20)
 dift <- tjan - toct #differenza jennaio meno ottobre 
 cldif <- colorRampPalette(c('blue','white','red'))(100) # 
 plot(dift, col=cldif)
+
+
+# zoomare australia 
+jan20 <-raster("firegen20.nc")
+ext <- c(110,160,-45,-10)
+zoom(jan20, ext=ext)
+cljan20 <- colorRampPalette(c('blue','white','red'))(100)  #CAMBIA STI COLORI BASTA!!
+jan20 <- crop(jan20, ext)
+plot(jan20, col=cljan20, main="titolo")
+
+
+
+### tentativo due ghiaccio 
+library(ncdf4)
+library(raster)
+
+setwd("C:/exam_m/") # windows
+
+# importing images #funzione raster 
+
+icejan21 <- raster("ice_2021.nc")  #prima immagine
+plot(icejan21)
+
+icejan???<- raster("c_gls_BA300_201901100000_GLOBE_PROBAV_V1.1.1.nc")  #seconda immagine 
+plot(jan19_fire)
+
+ext <- c(0,20,35,50)
+zoom(icejan21, ext=ext)
+
+cljan20 <- colorRampPalette(c('blue','white','red'))(100)  #CAMBIA STI COLORI BASTA!!
+jan20 <- crop(jan20, ext)
+plot(jan20, col=cljan20, main="titolo")
+
