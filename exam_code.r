9bdfe9ca421b2248ecb241517788e45d770b24a7
carlonotari <73799325+carlonotari@users.noreply.github.com>
2021-02-05 11:55:46 +0100

    Update exam_code.r

--- exam_code.r
+++ exam_code.r
@@ -11,8 +11,20 @@ library(raster)
 
 setwd("C:/exam_m/") # windows
 
-dec_fire <- raster("c_gls_BA300_201912100000_GLOBE_PROBAV_V1.1.1.nc")
-plot(dec_fire)
-march_fire <- raster("c_gls_BA300_202003310000_GLOBE_PROBAV_V1.1.1.nc")
-plot(march_fire)
+# importing images #funzione raster 
 
+jan20_fire <- raster("c_gls_BA300_202001100000_GLOBE_PROBAV_V1.1.1.nc")  #prima immagine
+plot(jan20_fire)
+
+jan19_fire <- raster("c_gls_BA300_201901100000_GLOBE_PROBAV_V1.1.1.nc")  #seconda immagine 
+plot(jan19_fire)
+
+# change the colorramppalette
+cljan20 <- colorRampPalette(c('blue','yellow','red'))(100) # 
+plot(jan20_fire, col=cljan20)
+
+plot(jan19_fire, col=cljan20)
+
+dift <- tjan - toct #differenza jennaio meno ottobre 
+cldif <- colorRampPalette(c('blue','white','red'))(100) # 
+plot(dift, col=cldif)
