ca18a2af207bd63bf584dee2cf5d96800f7ff72e
carlonotari <73799325+carlonotari@users.noreply.github.com>
2021-02-07 15:31:45 +0100

    Update exam_code.r

--- exam_code.r
+++ exam_code.r
@@ -7,126 +7,81 @@ library(ncdf4)
 library(raster)
 
 setwd("C:/exam_m/")
+#####
 
-al99<- raster("1999.nc")
-al20<- raster("2020.nc")
-plot(al99)
-#plot(al20)
+#visone del mondo ' 99 vs 2020
 
+al99 <- raster("1999.nc") 
+al20 <- raster("2020.nc")
 
-# tabella a quattro siberia prima e dopo e 
-# groellandia 99 vs 20
+clal <- colorRampPalette(c('blue','yellow','red'))(100)
 
-#zoom siberia  (40,80)
-#zoom groellandia (-150,-50)
-ext <- c(-150,-50,40,60)
-zoom(al99, ext=ext)
+par(mfrow=c(1,2))
 
-
-#tabella a 6: i 4 valori di prima e la loro differenza
-
-difal <- al99-al20  #differenza 99 meno 20
-
-cldif <- colorRampPalette(c('blue','white','red'))(100) # 
-plot(difal, col=cldif)
+plot(al99, col=clal, main="albedo during 1999")
+plot(al20, col=clal, main="albedo during 2020") 
 
 
-#zoom siberia  (40-80)
+##### fin qui bene!!!
 
-ext <- c(-150,150,40,150)
-zoom(al99, ext=ext)
-#zoom(al20, ext=ext)
+# siberia 99 vs siberia 20 e groellandia 99 vs groellandia 20 
 
-#al99 <- crop(al99, ext)## boh
+sib_al99 <- raster("1999.nc")
+ext <- c(0,150,40,60)
+zoom(sib_al99, ext=ext)
+clal <- colorRampPalette(c('blue','yellow','red'))(100)
+sib_al99 <- crop(sib_al99, ext)
 
-plot(al99)
-#plot(al20)
-#change the color
-
-clal99 <- colorRampPalette(c('blue','yellow','red'))(100)  #CAMBIA STI COLORI BASTA!!
-#sal99<- crop(al, ext)
-plot(al99, col=clal99, main="value of albedo during 1999")
-
-
-difal <- al99-al20  #differenza 99 meno 20
-
-cldif <- colorRampPalette(c('blue','white','red'))(100) # 
-plot(difal, col=cldif)
-
-#zoom groellandia 
-
-ext <- c(-150,-50,40,60)
-zoom(al99, ext=ext)
+sib_al20 <- raster("2020.nc")
+ext <- c(0,150,40,60)
+zoom(sib_al20, ext=ext)
+clal <- colorRampPalette(c('blue','yellow','red'))(100)
+sib_al20 <- crop(sib_al20, ext)
 
 
-#zoom siberia 
+gro_al99 <- raster("1999.nc")
+ext <- c(-130,-50,40,60)
+zoom(gro_al99, ext=ext)
+clal <- colorRampPalette(c('blue','yellow','red'))(100)
+gro_al99 <- crop(gro_al99, ext)
 
-#tabella a 4
+gro_al20 <- raster("2020.nc")
+ext <- c(-130,-50,40,60)
+zoom(gro_al20, ext=ext)
+clal <- colorRampPalette(c('blue','yellow','red'))(100)
+gro_al20 <- crop(gro_al20, ext)
 
 
+par(mfrow=c(2,2))
+
+plot(sib_al99, col=clal, main="siberia during 1999")
+plot(sib_al20, col=clal, main="siberia during 2020")
+plot(gro_al99, col=clal, main="greenland 1999")
+plot(gro_al29, col=clal, main="greenland 2020")
 
 
+# ha funzionatoooo
+
+########
+#plottare differenza albedo
+
+difs <- sib_al99 - sib_al20
+difg <- gro_al99 - gro_al20 
+
+cldif <-colorRampPalette(c('white','blue','red'))(100)
 
 
+par(mfrow=c(2,1))
 
-# https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
+plot(difs, col=cldif, main="differenza siberia") 
+plot(difg, col=cldif, main="differenza groellandia")
  
-# nc data: we need library
-# https://cran.r-project.org/web/packages/ncdf4/index.html
-# install.packages("ncdf4")
-library(ncdf4)
-library(raster)
 
-
-setwd("C:/exam_m/") # windows
-
-# importing images #funzione raster 
-
-jan20_fire <- raster("c_gls_BA300_202001100000_GLOBE_PROBAV_V1.1.1.nc")  #prima immagine
-plot(jan20_fire)
-
-jan19_fire <- raster("c_gls_BA300_201901100000_GLOBE_PROBAV_V1.1.1.nc")  #seconda immagine 
-plot(jan19_fire)
-
-# change the colorramppalette
-cljan20 <- colorRampPalette(c('blue','yellow','red'))(100) # 
-plot(jan20_fire, col=cljan20)
-
-plot(jan19_fire, col=cljan20)
-
-dift <- tjan - toct #differenza jennaio meno ottobre 
-cldif <- colorRampPalette(c('blue','white','red'))(100) # 
-plot(dift, col=cldif)
-
-
-# zoomare australia 
-jan20 <-raster("firegen20.nc")
-ext <- c(110,160,-45,-10)
-zoom(jan20, ext=ext)
-cljan20 <- colorRampPalette(c('blue','white','red'))(100)  #CAMBIA STI COLORI BASTA!!
-jan20 <- crop(jan20, ext)
-plot(jan20, col=cljan20, main="titolo")
+## non ci credo: si può fareeeeee
+#img:frankestain junior  
+############
 
 
 
-### tentativo due ghiaccio 
-library(ncdf4)
-library(raster)
 
-setwd("C:/exam_m/") # windows
-
-# importing images #funzione raster 
-
-icejan21 <- raster("ice_2021.nc")  #prima immagine
-plot(icejan21)
-
-icejan???<- raster("c_gls_BA300_201901100000_GLOBE_PROBAV_V1.1.1.nc")  #seconda immagine 
-plot(jan19_fire)
-
-ext <- c(0,20,35,50)
-zoom(icejan21, ext=ext)
-
-cljan20 <- colorRampPalette(c('blue','white','red'))(100)  #CAMBIA STI COLORI BASTA!!
-jan20 <- crop(jan20, ext)
-plot(jan20, col=cljan20, main="titolo")
 
