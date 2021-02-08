1769a45df5c3297dfad6eee49a21fb3eae9eca95
carlonotari <73799325+carlonotari@users.noreply.github.com>
2021-02-08 11:26:43 +0100

    Update exam_code.r

--- exam_code.r
+++ exam_code.r
@@ -11,41 +11,41 @@ setwd("C:/exam_m/")
 
 #visone del mondo ' 99 vs 2020
 
-al99 <- raster("1999.nc") 
-al20 <- raster("2020.nc")
+al99 <- raster("j1999.nc")        # https://land.copernicus.vgt.vito.be/PDF/free?productID=30608047&collectionID=1000174&fileIndex=0
+al20 <- raster("j2020.nc")        # https://land.copernicus.vgt.vito.be/PDF/free?productID=56032676&collectionID=1000174&fileIndex=0
 
 clal <- colorRampPalette(c('blue','yellow','red'))(100)
 
 par(mfrow=c(1,2))
 
-plot(al99, col=clal, main="albedo during 1999")
-plot(al20, col=clal, main="albedo during 2020") 
+plot(al99, col=clal, main="albedo in january 1999")
+plot(al20, col=clal, main="albedo in january 2020") 
 
 
 ##### fin qui bene!!!
 
 # siberia 99 vs siberia 20 e groellandia 99 vs groellandia 20 
 
-sib_al99 <- raster("1999.nc")
+sib_al99 <- raster("j1999.nc")
 ext <- c(0,150,40,60)
 zoom(sib_al99, ext=ext)
 clal <- colorRampPalette(c('blue','yellow','red'))(100)
 sib_al99 <- crop(sib_al99, ext)
 
-sib_al20 <- raster("2020.nc")
+sib_al20 <- raster("j2020.nc")
 ext <- c(0,150,40,60)
 zoom(sib_al20, ext=ext)
 clal <- colorRampPalette(c('blue','yellow','red'))(100)
 sib_al20 <- crop(sib_al20, ext)
 
 
-gro_al99 <- raster("1999.nc")
+gro_al99 <- raster("j1999.nc")
 ext <- c(-130,-50,40,60)
 zoom(gro_al99, ext=ext)
 clal <- colorRampPalette(c('blue','yellow','red'))(100)
 gro_al99 <- crop(gro_al99, ext)
 
-gro_al20 <- raster("2020.nc")
+gro_al20 <- raster("j2020.nc")
 ext <- c(-130,-50,40,60)
 zoom(gro_al20, ext=ext)
 clal <- colorRampPalette(c('blue','yellow','red'))(100)
@@ -54,10 +54,10 @@ gro_al20 <- crop(gro_al20, ext)
 
 par(mfrow=c(2,2))
 
-plot(sib_al99, col=clal, main="siberia during 1999")
-plot(sib_al20, col=clal, main="siberia during 2020")
-plot(gro_al99, col=clal, main="greenland 1999")
-plot(gro_al29, col=clal, main="greenland 2020")
+plot(sib_al99, col=clal, main="Siberia 1999")
+plot(sib_al20, col=clal, main="Siberia 2020")
+plot(gro_al99, col=clal, main="Greenland 1999")
+plot(gro_al20, col=clal, main="Greenland 2020")
 
 
 # ha funzionatoooo
@@ -73,8 +73,8 @@ cldif <-colorRampPalette(c('white','blue','red'))(100)
 
 par(mfrow=c(2,1))
 
-plot(difs, col=cldif, main="differenza siberia") 
-plot(difg, col=cldif, main="differenza groellandia")
+plot(difs, col=cldif, main="variation Siberia") 
+plot(difg, col=cldif, main="variation Greenland")
  
 
 ## non ci credo: si può fareeeeee
