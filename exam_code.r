6708563a458db2d7afe044d190e0d890a5403415
carlonotari <73799325+carlonotari@users.noreply.github.com>
2021-02-09 17:51:04 +0100

    Update exam_code.r

--- exam_code.r
+++ exam_code.r
@@ -2,21 +2,21 @@
 # https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
 # https://cran.r-project.org/web/packages/ncdf4/index.html
 
-
+#library 
 library(ncdf4)
 library(raster)
 
-setwd("C:/exam_m/")
+setwd("C:/exam_m/")  #setting the working directory
 #####
 
 #visone del mondo ' 99 vs 2020
 
-al99 <- raster("j1999.nc")        # https://land.copernicus.vgt.vito.be/PDF/free?productID=30608047&collectionID=1000174&fileIndex=0
-al20 <- raster("j2020.nc")        # https://land.copernicus.vgt.vito.be/PDF/free?productID=56032676&collectionID=1000174&fileIndex=0
+al99 <- raster("j1999.nc")     #importing images   # https://land.copernicus.vgt.vito.be/PDF/free?productID=30608047&collectionID=1000174&fileIndex=0
+al20 <- raster("j2020.nc")     #importing images   # https://land.copernicus.vgt.vito.be/PDF/free?productID=56032676&collectionID=1000174&fileIndex=0
 
-clal <- colorRampPalette(c('darkslateblue','firebrick1','gold1'))(100)
+clal <- colorRampPalette(c('darkslateblue','firebrick1','gold1'))(100)   #it changes the color of the map 
 
-par(mfrow=c(1,2))
+par(mfrow=c(1,2))   #par is used to put multiple graphs in a single plot # mfrow stays for multiframe row in this case # 1 rows and 2 columns
 
 plot(al99, col=clal, main="albedo in january 1999")
 plot(al20, col=clal, main="albedo in january 2020") 
@@ -26,33 +26,33 @@ plot(al20, col=clal, main="albedo in january 2020")
 
 # siberia 99 vs siberia 20 e groellandia 99 vs groellandia 20 
 
-sib_al99 <- raster("j1999.nc")
-ext <- c(0,150,40,60)
+sib_al99 <- raster("j1999.nc")  #importing images 
+ext <- c(0,150,40,60)              
 zoom(sib_al99, ext=ext)
 clal <- colorRampPalette(c('darkslateblue','firebrick1','gold1'))(100)
 sib_al99 <- crop(sib_al99, ext)
 
-sib_al20 <- raster("j2020.nc")
+sib_al20 <- raster("j2020.nc") #importing images
 ext <- c(0,150,40,60)
 zoom(sib_al20, ext=ext)
 clal <- colorRampPalette(c('darkslateblue','firebrick1','gold1'))(100)
 sib_al20 <- crop(sib_al20, ext)
 
 
-gro_al99 <- raster("j1999.nc")
+gro_al99 <- raster("j1999.nc")  #importing images
 ext <- c(-130,-50,40,60)
 zoom(gro_al99, ext=ext)
 clal <- colorRampPalette(c('darkslateblue','firebrick1','gold1'))(100)
 gro_al99 <- crop(gro_al99, ext)
 
-gro_al20 <- raster("j2020.nc")
+gro_al20 <- raster("j2020.nc") #importing images
 ext <- c(-130,-50,40,60)
 zoom(gro_al20, ext=ext)
 clal <- colorRampPalette(c('darkslateblue','firebrick1','gold1'))(100)
 gro_al20 <- crop(gro_al20, ext)
 
 
-par(mfrow=c(2,2))
+par(mfrow=c(2,2))    #2 rows and 2 columns
 
 plot(sib_al99, col=clal, main="Siberia 1999")
 plot(sib_al20, col=clal, main="Siberia 2020")
@@ -68,7 +68,7 @@ plot(gro_al20, col=clal, main="Greenland 2020")
 difs <- sib_al99 - sib_al20
 difg <- gro_al99 - gro_al20 
 
-cldif <-colorRampPalette(c('white','firebrick2','gold1'))(100)
+cldif <-colorRampPalette(c('white','blue','gold1'))(100)
 
 
 par(mfrow=c(2,1))
@@ -83,6 +83,71 @@ plot(difg, col=cldif, main="variation Greenland")
 ############
 
 
+library(ncdf4)
+library(raster)
 
+setwd("C:/exam_m/")
+
+al99 <- raster("l2000.nc")     #importing images   # https://land.copernicus.vgt.vito.be/PDF/free?productID=30608047&collectionID=1000174&fileIndex=0
+al20 <- raster("l2019.nc")     #importing images   # https://land.copernicus.vgt.vito.be/PDF/free?productID=56032676&collectionID=1000174&fileIndex=0
+
+clal <- colorRampPalette(c('darkslateblue','firebrick1','gold1'))(100)   #it changes the color of the map 
+
+par(mfrow=c(1,2))   #par is used to put multiple graphs in a single plot # mfrow stays for multiframe row in this case # 1 rows and 2 columns
+
+plot(al99, col=clal, main="albedo in july 1999")
+plot(al20, col=clal, main="albedo in july 2020") 
+
+
+
+sib_al99 <- raster("l2000.nc")  #importing images 
+ext <- c(0,150,40,60)              
+zoom(sib_al99, ext=ext)
+clal <- colorRampPalette(c('darkslateblue','firebrick1','gold1'))(100)
+sib_al99 <- crop(sib_al99, ext)
+
+sib_al20 <- raster("l2019.nc") #importing images
+ext <- c(0,150,40,60)
+zoom(sib_al20, ext=ext)
+clal <- colorRampPalette(c('darkslateblue','firebrick1','gold1'))(100)
+sib_al20 <- crop(sib_al20, ext)
+
+
+gro_al99 <- raster("l2000.nc")  #importing images
+ext <- c(-130,-50,40,60)
+zoom(gro_al99, ext=ext)
+clal <- colorRampPalette(c('darkslateblue','firebrick1','gold1'))(100)
+gro_al99 <- crop(gro_al99, ext)
+
+gro_al20 <- raster("l2019.nc") #importing images
+ext <- c(-130,-50,40,60)
+zoom(gro_al20, ext=ext)
+clal <- colorRampPalette(c('darkslateblue','firebrick1','gold1'))(100)
+gro_al20 <- crop(gro_al20, ext)
+
+
+par(mfrow=c(2,2))    #2 rows and 2 columns
+
+plot(sib_al99, col=clal, main="Siberia 1999")
+plot(sib_al20, col=clal, main="Siberia 2020")
+plot(gro_al99, col=clal, main="Greenland 1999")
+plot(gro_al20, col=clal, main="Greenland 2020")
+
+
+
+
+#####
+
+
+difs <- sib_al99 - sib_al20
+difg <- gro_al99 - gro_al20 
+
+cldif <-colorRampPalette(c('blue','white','red'))(100)
+
+
+par(mfrow=c(2,1))
+
+plot(difs, col=cldif, main="variation Siberia") 
+plot(difg, col=cldif, main="variation Greenland")
 
 
