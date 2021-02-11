b4f82378a702412f449a6b6cbe8b0fc2c6a9ca6e
carlonotari <73799325+carlonotari@users.noreply.github.com>
2021-02-11 21:45:07 +0100

    Update R_first.r

--- R_first.r
+++ R_first.r
@@ -1,12 +1,12 @@
-> # write whatever you want
-> 
-> # this is an array
-> primates <- c(3, 5, 9, 15, 40)
-> primates
+# write whatever you want
+
+# this is an array
+primates <- c(3, 5, 9, 15, 40)
+primates
 [1]  3  5  9 15 40
-> 
-> # new set:
-> crabs <- c(100, 70, 30, 10, 5)
+
+# new set:
+crabs <- c(100, 70, 30, 10, 5)
 crabs
 [1] 100  70  30  10   5
 
@@ -14,22 +14,22 @@ crabs
 
 plot( primates, crabs)
 
-plot( primates, crabs, col="red")  #cambia il colore del pallino
-plot (primates, crabs, col="red", pch=19)       #cambia la tipologia del pallino
+plot( primates, crabs, col="red")  #cambia il colore del pallino # change the dots color
+plot (primates, crabs, col="red", pch=19)       #cambia la tipologia del pallino  #change the type of the dots 
 primates <- c(3, 5, 9, 15, 40)
-> 
-> primates
+
+primates
 [1]  3  5  9 15 40
-> crabs <- c(100, 70, 30, 10, 5)
-> 
-> crabs
+crabs <- c(100, 70, 30, 10, 5)
+ 
+crabs
 [1] 100  70  30  10   5
-> 
-> plot(primates, crabs)
-> plot(primates, crabs, col= "red")    # it changes the colour of "pallinno"
-> plot(primates, crabs, col= "red", pch=19, cex=6) # it changes the dimensions and the type of "pallinno"
-> plot(primates, crabs, col= "red", pch=19, cex=6, main=" My first ecologycal graph in R")  #main "titolo"
-> 
+
+plot(primates, crabs)
+plot(primates, crabs, col= "red")    # it changes the colour of "pallinno"
+plot(primates, crabs, col= "red", pch=19, cex=6) # it changes the dimensions and the type of "pallinno"
+plot(primates, crabs, col= "red", pch=19, cex=6, main=" My first ecologycal graph in R")  #main "titolo"
+
 # create a dataframe che è la tabella   
 ecoset <- data.frame(primates, crabs)
 # ecological numbers 
